d936bad7031625cc6db8bb2657d4af2c4cd0a936
elinx <xizzuli@163.com>
2016-10-22 23:41:50 +0800

    remove files

--- init.el
+++ init.el
@@ -251,7 +251,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight semi-bold :height 143 :width normal)))))
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 120 :width normal)))))
 
 
 ;;; shell
