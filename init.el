554e46946a759bc89a2b69f5bf149a6c8858874f
elinx <xizzuli@163.com>
2016-10-25 22:55:48 +0800

    Make irony work

--- init.el
+++ init.el
@@ -7,8 +7,8 @@
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
 (load-theme 'solarized t)
-(set-frame-parameter nil 'background-mode 'light)
-;;(set-frame-parameter nil 'background-mode 'dark)
+;; (set-frame-parameter nil 'background-mode 'light)
+(set-frame-parameter nil 'background-mode 'dark)
 ;; (add-hook 'after-make-frame-functions
 ;;           (lambda (frame)
 ;;             (let ((mode (if (display-graphic-p frame) 'dark 'light)))
@@ -82,7 +82,15 @@
 (require 'irony)
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
-
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
 
 ;;; company mode
