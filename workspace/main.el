3aad0455859f0a6f3a5d8336ad7bdb70d66714e4
elinx <xizzuli@163.com>
2017-03-14 00:10:48 +0800

    jump to the end of the word selected

--- workspace/main.el
+++ workspace/main.el
@@ -1,15 +1,22 @@
 ;; list words of current line using helm framework
+(defvar elinx/words-alist '())
+
 (defun elinx/words-of-current-line ()
   (save-excursion
     (with-helm-current-buffer
       (goto-char (line-beginning-position))
       (cl-loop while (re-search-forward "\\b\\w+\\b" (line-end-position) t)
-        collect (match-string 0)))))
+        collect (list (match-string 0) (point))))))
+
+(defun elinx/jump-to-word (candidate)
+  (with-helm-current-buffer
+    (goto-char (car candidate))))
 
 (defun elinx/list-words-of-current-line ()
   (interactive)
   (helm :sources (helm-build-sync-source "Words of Current Line"
-                  :candidates 'elinx/words-of-current-line)
+                   :candidates 'elinx/words-of-current-line
+                   :action 'elinx/jump-to-word)
         :buffer "*Words of Current Line*"))
 
 (global-set-key (kbd "C-x w") 'elinx/list-words-of-current-line)
