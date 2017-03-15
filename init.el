59af60c4c28b1a6daccc939f4c45bc8a0dfcb891
elinx <xizzuli@163.com>
2017-03-15 23:08:30 +0800

    elpy & nyan & lambda

--- init.el
+++ init.el
@@ -157,6 +157,9 @@
  '(custom-safe-themes
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(elpy-modules
+   (quote
+    (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
  '(helm-gtags-auto-update t)
  '(helm-gtags-ignore-case t)
  '(helm-gtags-path-style (quote relative))
@@ -338,3 +341,9 @@
 (add-to-list 'default-frame-alist '(left-fringe . 0))
 
 (which-function-mode 1)
+
+(require 'package)
+(elpy-enable)
+
+(nyan-mode t)
+(prettify-symbols-mode t)
