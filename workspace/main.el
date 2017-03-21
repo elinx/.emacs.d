96481ddab9e50d3f78eec3382258494899aa62f6
Elinx Hsi <xizzuli@163.com>
2017-03-21 14:59:40 +0800

    Change M-f and M-b behavior

--- workspace/main.el
+++ workspace/main.el
@@ -1,10 +1,14 @@
 (require 'wordjump)
 (require 'utils)
+(require 'misc)
 
 (global-set-key (kbd "M-g e") 'elinx/list-words-of-current-line)
 (global-set-key (kbd "C-a") 'elinx/jump-to-beg-of-line-or-first-character)
 (global-set-key (kbd "C-w") 'elinx/kill-region-or-current-line)
 (global-set-key (kbd "M-DEL") 'elinx/delete-till-the-beginning-of-line)
 (global-set-key (kbd "M-w") 'elinx/copy-region-or-current-line)
+
+(global-set-key (kbd "M-f") 'forward-to-word)
+(global-set-key (kbd "M-b") 'backward-to-word)
 
 (provide 'main)
